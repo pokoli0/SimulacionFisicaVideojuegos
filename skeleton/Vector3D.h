0dc2300269075f5df9bd60eed752903d67f18608
pokoli0 <psierr01@ucm.es>
2024-09-20 10:43:24 +0200

    Update Vector3D.h

--- skeleton/Vector3D.h
+++ skeleton/Vector3D.h
@@ -1,12 +1,52 @@
+#include <cmath>
+
 #pragma once
+
+using namespace std;
+
 class Vector3D
 {
-	Vector3D(float x, float y, float z) {
+public:
+
+	Vector3D(float x_, float y_, float z_) {
 		x = x_;
 		y = y_;
 		z = z_;
 	};
 
-	float x_, y_, z_;
+#pragma region getters & setters
+	void setX(float n) { x_ = n;}
+	void setY(float n) { y_ = n;}
+	void setZ(float n) { z_ = n;}
+	float getX() { return x_;}
+	float getY() { return y_;}
+	float getZ() { return z_;}
+#pragma endregion
+
+	// magnitud
+	float modulo() {
+		return sqrt(x * x + y * y + z * z);
+	}
+
+	// dividir cada componente entre el modulo
+	void normaliza() {
+		float mod = modulo();
+		if (mod != 0) {
+			x /= mod;
+			y /= mod;
+			z /= mod;
+		}
+	}
+	
+	float prodEscalar(Vector3D& v) {
+		return(x * v.x + y * v.y + z * v.z);
+	}
+
+
+
+
+private:
+
+	float x, y, z;
 };
 
