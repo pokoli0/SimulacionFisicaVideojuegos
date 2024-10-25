dcd2622162e89d00d18f53533a9fc3ed145fd872
pokoli <psierr01@ucm.es>
2024-10-26 00:02:24 +0200

    fuente al fin

--- skeleton/Particle.cpp
+++ skeleton/Particle.cpp
@@ -3,23 +3,46 @@
 #include <iostream>
 #include "ParticleSystem.h"
 
-Particle::Particle(PxVec3 pos, PxVec3 velo, PxVec3 accele) : pose(pos), vel(velo), accel(accele)
+Particle::Particle(PxVec3 pos, PxVec3 velo, PxVec3 accele)
+	: transform(pos), vel(velo), accel(accele)
 {
 	damping = 0.99;
-	pose = physx::PxTransform(pos);
-	
-	accel = Vector3(0, 0, 0);
-	lifeTime = 0.0;
+	transform = physx::PxTransform(pos);
 
-	vel = velo;
-	accel = accele;
+	PxSphereGeometry geo(1);
+	PxShape* shape = CreateShape(geo);
 
-	alive = true;
-
-	PxShape* shape = CreateShape(PxSphereGeometry(1));
-	renderItem = new RenderItem(shape, &pose, PxVec4(0,0,0,1));
-
+	renderItem = new RenderItem(shape, &transform, Vector4(1, 0.5, 1, 1));
 	RegisterRenderItem(renderItem);
+	damping = 0.99;
+}
+
+Particle::Particle(PxVec3 pos, PxVec3 velo, PxVec3 accele, double lifetime)
+	: transform(pos), vel(velo), accel(accele), lifeTime(lifetime)
+{
+	damping = 0.99;
+	transform = physx::PxTransform(pos);
+	
+	PxSphereGeometry geo(1);
+	PxShape* shape = CreateShape(geo);
+
+	renderItem = new RenderItem(shape, &transform, Vector4(1, 0.5, 1, 1));
+	RegisterRenderItem(renderItem);
+	damping = 0.99;
+}
+
+Particle::Particle(Particle const& p)
+{
+	vel = p.vel;
+	accel = p.accel;
+	transform = PxTransform(p.transform.p);
+	PxShape* shape = CreateShape(PxSphereGeometry(1));
+	renderItem = new RenderItem(shape, &transform, Vector4(1, 0.5, 1, 1));
+
+	center = p.center;
+	ratio = p.ratio;
+	lifeTime = p.lifeTime;
+	damping = 0.99;
 }
 
 Particle::~Particle()
@@ -34,26 +57,31 @@ void Particle::Integrate(double t, IntegrationType type)
 	if (type == EULER) {
 		vel = vel + accel * t;
 		vel = vel * pow(damping, t);
-		pose.p = pose.p + vel * t;
+		transform.p = transform.p + vel * t;
 	}
 	else { // semieuler
-		pose.p = pose.p + vel * t;
-		vel = vel + accel * t;
-		vel = vel * pow(damping, t);
+		vel = vel * pow(damping, t) + accel * t;
+		transform.p = transform.p + vel * t;
 	}
 }
 
 void Particle::isAlive(double t, ParticleSystem& system, IntegrationType type)
 {
-	timeAlive += t;
 	Integrate(t, type);
-	if (timeAlive > lifeTime || !isOnRatio()) { // ver donde se modifica lifetime
+	if (updateLifeTime(t) || !isOnRatio()) { // ver donde se modifica lifetime
 		system.destroyParticle(this);
-		alive = false;
 	}
 }
 
+bool Particle::updateLifeTime(double t)
+{
+	lifeTime -= t;
+	return (lifeTime <= 0);
+
+}
 bool Particle::isOnRatio()
 {
-	return (pose.p - center).magnitude() < ratio;
+	return (transform.p - center).magnitude() < ratio;
 }
+
+
--- skeleton/Particle.h
+++ skeleton/Particle.h
@@ -16,23 +16,32 @@ public:
 	};
 
 	Particle(PxVec3 pos, PxVec3 velo, PxVec3 accele);
-	virtual ~Particle();
+
+	Particle(PxVec3 pos, PxVec3 velo, PxVec3 accele, double lifetime);
+	Particle(Particle const& p);
+	~Particle();
+
 
 	void Integrate(double t, IntegrationType type);
 	void isAlive(double t, ParticleSystem& system, IntegrationType type);
 
 	bool isOnRatio();
 
+	bool updateLifeTime(double t);
+
 	// getters & setters
 	double getLifeTime() const { return lifeTime; }
 	void setLifeTime(double t) { lifeTime = t; }
+
 	list<Particle*>::iterator getIterator() const { return iterator; }
 	void setIterator(list<Particle*>::iterator i) { iterator = i; }
 
-	PxVec3 getPosition() const { return pose.p; }
-	void setPosition(PxVec3 p) { pose.p = p; }
+	void setPosition(PxVec3 p) { transform.p = p; }
+	PxVec3 getPosition() { return transform.p; }
+
 	void setVelocity(PxVec3 v) { vel = v; }
 	PxVec3 getVelocity() const { return vel; }
+
 	void setAcceleration(PxVec3 a) { accel = a; }
 
 	void setRatio(float r) { ratio = r; }
@@ -42,9 +51,9 @@ protected:
 
 	PxVec3 vel;
 	PxVec3 accel;
-	PxTransform pose;
+	//PxTransform pose;
 
-	PxTransform* transform = nullptr;
+	PxTransform transform;
 
 	double damping;
 
@@ -53,9 +62,7 @@ protected:
 	double mass;
 
 	double lifeTime;
-	double timeAlive;
-
-	bool alive;
+	//double timeAlive;
 
 	PxVec3 center;
 	float ratio;
